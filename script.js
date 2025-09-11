c455f1023d8e0b976e10fa24cda943da8b74eda9
mariazinhalins <maria.santos.lins@escola.pr.gov.br>
2025-09-11 08:44:15 -0300

    ok

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ avanca.forEach(button => {
 button.addEventListener('click',function(){
     const atual = document.querySelector('.ativo');
     const proximoPasso='passo-'+this.getAttribute('data-proximo');
-    
+
     atual.classList.remove('ativo');
     const proximoElemento= document.getElementById(proximoPasso);
 
@@ -17,7 +17,5 @@ button.addEventListener('click',function(){
     }
 
 
-
   })
-}
-);
+  });
